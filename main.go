1504562627495cd984f0f15c68fc116e288560b7
jbdoumenjou <925513+jbdoumenjou@users.noreply.github.com>
2023-11-24 15:13:39 +0100

    clean

--- main.go
+++ main.go
@@ -18,29 +18,24 @@ func (t toto) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 func main() {
 	mux := http.NewServeMux()
 
-	mux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
-		log.Printf("serving %s\n", r.Host+r.RequestURI)
-		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
-		w.WriteHeader(http.StatusOK)
-		w.Write([]byte("OK"))
-	})
+	mux.HandleFunc("/healthz", healthzHandler)
 
 	appHandler := http.StripPrefix("/app", http.FileServer(http.Dir(".")))
 	apiMetrics := &apiConfig{}
 	mux.Handle("/app/", apiMetrics.middlewareMetricsInc(appHandler))
-	mux.HandleFunc("/metrics", func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
-		w.WriteHeader(http.StatusOK)
-		w.Write([]byte("Hits: " + fmt.Sprintf("%d", apiMetrics.fileserverHits)))
-	})
-	mux.HandleFunc("/reset", func(w http.ResponseWriter, r *http.Request) {
-		w.WriteHeader(http.StatusOK)
-		apiMetrics.fileserverHits = 0
-	})
+	mux.HandleFunc("/metrics", apiMetrics.metricsHandler)
+	mux.HandleFunc("/reset", apiMetrics.resetHandler)
 
 	corsmux := middlewareCors(mux)
 
 	log.Fatal(NewWebServer(":8080", corsmux).Start())
+}
+
+func healthzHandler(w http.ResponseWriter, r *http.Request) {
+	log.Printf("serving %s\n", r.Host+r.RequestURI)
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("OK"))
 }
 
 func middlewareCors(next http.Handler) http.Handler {
@@ -58,6 +53,17 @@ func middlewareCors(next http.Handler) http.Handler {
 
 type apiConfig struct {
 	fileserverHits int
+}
+
+func (cfg *apiConfig) metricsHandler(w http.ResponseWriter, _ *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("Hits: " + fmt.Sprintf("%d", cfg.fileserverHits)))
+}
+
+func (cfg *apiConfig) resetHandler(w http.ResponseWriter, _ *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	cfg.fileserverHits = 0
 }
 
 func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
